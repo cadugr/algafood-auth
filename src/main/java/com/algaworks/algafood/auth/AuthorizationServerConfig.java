5583a3493b076c30f28144e354c917a0f57f242c
cadugr <cadu.guerra@gmail.com>
2022-01-17 21:28:16 -0300

    Alterando a secret para gerar assinatura.

--- src/main/java/com/algaworks/algafood/auth/AuthorizationServerConfig.java
+++ src/main/java/com/algaworks/algafood/auth/AuthorizationServerConfig.java
@@ -85,7 +85,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Bean
 	public JwtAccessTokenConverter jwtAccessTokenConverter() {
 		JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
-		jwtAccessTokenConverter.setSigningKey("algaworks"); //definindo a chave pra uso da criptografia HS256 (simétrica).
+		jwtAccessTokenConverter.setSigningKey("89a7sd89f7as98f7dsa98fds7fd89sasd9898asdf98s"); //definindo a chave pra uso da criptografia HS256 (simétrica).
 		return jwtAccessTokenConverter; 
 	}
 	
