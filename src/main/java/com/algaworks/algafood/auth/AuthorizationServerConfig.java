36652c8e8d39346623df26d3fa11ba91f56dc4bd
cadugr <cadu.guerra@gmail.com>
2022-01-18 11:55:52 -0300

    Configurando a validação de JWT no Resource Server com a chave pública.

--- src/main/java/com/algaworks/algafood/auth/AuthorizationServerConfig.java
+++ src/main/java/com/algaworks/algafood/auth/AuthorizationServerConfig.java
@@ -73,6 +73,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
 		security.checkTokenAccess("permitAll()")
+				.tokenKeyAccess("permitAll()")
 				.allowFormAuthenticationForClients();
 	}
 	
